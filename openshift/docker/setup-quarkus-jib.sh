ecf6ad2983631cf6a518c5cac2590aee3737b964
Barry Lulas <barry.lulas@ibm.com>
2025-07-30 20:39:31 -0400

    updated

---
+++ openshift/docker/setup-quarkus-jib.sh
@@ -0,0 +1,9 @@
+echo "Logging into Docker so that Quarkus JIB image builds work from pipelines and developer workstation..."
+
+echo "Logging into OCP and setting default project..."
+oc login --username=ocadmin --password=ibmrhocp https://api.ocp.ibm.edu:6443
+oc project bamoe-apps
+
+echo "Logging into Docker using new token..."
+HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
